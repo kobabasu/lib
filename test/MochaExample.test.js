a5cd22f982fab6afe42d608f1e371a5a49a05377
kobabasu <keiji.kobayashi.web@gmail.com>
2017-09-09 18:28:22 +0900

    phantomjs-node用に書き換え

--- test/MochaExample.test.js
+++ test/MochaExample.test.js
@@ -1,33 +1,41 @@
 var assert = require('chai').assert;
 var phantom = require('phantom');
 var url = './MochaExample.test.html';
+var src = './src/mocha-example.js';
 var _ph, _page;
 
 describe('MochaExample', function() {
 
-  it('ブラウザの幅が1920pxか', function() {
-    return phantom.create().then(function(ph) {
+  it('結果が3となるか', function() {
+    var promise = phantom.create().then(function(ph) {
       _ph = ph;
       return ph.createPage();
     }).then(function(page) {
       _page = page;
-      _page.property('viewportSize', {
-        width: 1920,
-        height: 1080
-      });
-
       return _page.open(url);
     }).then(function(status) {
-      return _page.evaluate(function() {
-        return window.innerWidth;
-      });
-    }).then(function(html) {
-      _page.close().then(function() {
-        _ph.exit();
+      return _page.injectJs(src);
+    }).then(function(loaded) {
+
+      // evaluate start
+      var change =  _page.evaluate(function() {
+        var module = new MochaExample();
+        return module.change();
+      }).then(function(res) {
+        assert.equal(res, 3);
       });
 
-      assert.equal(html, 1920);
+      return Promise.all([change]);
     });
+
+    return promise;
   });
 
+  after(function(done) {
+    _page.close().then(function() {
+      _ph.exit();
+    });
+
+    done();
+  });
 });
