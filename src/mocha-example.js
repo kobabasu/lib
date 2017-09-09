bf23f9f8b371b2b23227db7de512111f60435c2e
kobabasu <keiji.kobayashi.web@gmail.com>
2017-09-09 09:08:09 +0900

    いらないファイルを削除

--- src/mocha-example.js
+++ src/mocha-example.js
@@ -33,35 +33,3 @@
 
   return MochaExample;
 });
-
-
-/**
- * MochaExampleTwo
- *
- * @return {void}
- */
-(function(global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(factory(global));
-  } else if (typeof exports === 'object') {
-    module.exports.MochaExampleTwo = factory(global);
-  } else {
-    MochaExampleTwo = factory(global);
-  }
-})((this || 0).self || global, function(global) {
-  'use strict';
-
-  function MochaExampleTwo() {
-  }
-
-  MochaExampleTwo.prototype = Object.create(Object.prototype, {
-    'constructor': { 'value': MochaExampleTwo },
-    'change': { 'value': MochaExampleTwo_change }
-  });
-
-  function MochaExampleTwo_change() {
-    return 4;
-  };
-
-  return MochaExampleTwo;
-});
--- test/MochaExample.test.js
+++ test/MochaExample.test.js
@@ -1,17 +1,33 @@
-var global = (function() { return this })();
-if (!('chai' in global)) {
-  var chai = require('chai');
-  var sinon = require('sinon');
-  var assert = chai.assert;
-
-  var module = require('../src/mocha-example.js');
-  var mochaExample = new module.MochaExample();
-};
+var assert = require('chai').assert;
+var phantom = require('phantom');
+var url = './MochaExample.test.html';
+var _ph, _page;
 
 describe('MochaExample', function() {
 
-  it('結果が3となるか', function() {
-    assert.equal(mochaExample.change(), 3);
+  it('ブラウザの幅が1920pxか', function() {
+    return phantom.create().then(function(ph) {
+      _ph = ph;
+      return ph.createPage();
+    }).then(function(page) {
+      _page = page;
+      _page.property('viewportSize', {
+        width: 1920,
+        height: 1080
+      });
+
+      return _page.open(url);
+    }).then(function(status) {
+      return _page.evaluate(function() {
+        return window.innerWidth;
+      });
+    }).then(function(html) {
+      _page.close().then(function() {
+        _ph.exit();
+      });
+
+      assert.equal(html, 1920);
+    });
   });
 
 });
