2d30622623af584ecc83fdcc2f95d89ed7e3f110
kobabasu <keiji.kobayashi.web@gmail.com>
2017-09-24 11:32:24 +0900

    おおまかな箇所をアロー記法に変更

--- test/Chrome.test.js
+++ test/Chrome.test.js
@@ -1,18 +1,18 @@
-var fs = require('fs');
-var assert = require('chai').assert;
-var chromelauncher = require('chrome-launcher');
-var CDP = require('chrome-remote-interface');
+import fs from 'fs'
+import { assert } from 'chai'
+import { launch } from 'chrome-launcher'
+import CDP from 'chrome-remote-interface'
 
-var html = './test/Chrome.test.html';
-// var js = './modules/UpdateCopyright.js';
+const HTML = './test/Chrome.test.html';
+// const JS = './modules/UpdateCopyright.js';
 
-function fetch(filename) {
+const fetch = (filename) => {
   return fs.readFileSync(filename, 'utf-8');
 }
 
-async function startHeadlessChrome() {
+const startHeadlessChrome = async () => {
   try {
-    return await chromelauncher.launch({
+    return await launch({
       port: 9222,
       startingUrl: 'target:blank',
       chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox']
@@ -22,45 +22,44 @@ async function startHeadlessChrome() {
   }
 }
 
-describe('chrome-headless-sample', function() {
-  it('titleを評価できるか', function(done) {
+describe('chrome-headless-sample', () => {
+  it('titleを評価できるか', (done) => {
 
-    startHeadlessChrome().then(function(chrome) {
+    startHeadlessChrome().then(async (chrome) => {
 
-      CDP(async function (client) {
-        var Page = client.Page;
-        var Runtime = client.Runtime;
-        await Page.enable();
-        await Runtime.enable();
+      const client = await CDP({ port: chrome.port });
+      const { Page, Runtime, Console } = client;
+      await Page.enable();
+      await Runtime.enable();
+      await Console.enable();
 
-        var blank = await Page.navigate({ url: 'target:blank' });
-        await Page.setDocumentContent({
-          frameId: blank.frameId,
-          html: fetch(html)
-        });
-
-        // await Page.addScriptToEvaluateOnLoad({
-        //   scriptSource: js
-        // });
-
-        // await Page.loadEventFired();
-
-        var exp = `document.querySelector('title').innerHTML`;
-        var title = await Runtime.evaluate({
-          expression: exp 
-        });
-
-        try {
-          assert.equal(title.result.value, 'sample');
-        } catch(error) {
-          return done(error);
-        } finally {
-          client.close();
-          chrome.kill();
-        }
-
-        done();
+      const blank = await Page.navigate({ url: 'target:blank' });
+      await Page.setDocumentContent({
+        frameId: blank.frameId,
+        html: fetch(HTML)
       });
+
+      // await Page.addScriptToEvaluateOnLoad({
+      //   scriptSource: JS
+      // });
+
+      // await Page.loadEventFired();
+
+      var exp = `document.querySelector('title').innerHTML`;
+      var title = await Runtime.evaluate({
+        expression: exp 
+      });
+
+      try {
+        assert.equal(title.result.value, 'sample');
+      } catch(error) {
+        return done(error);
+      } finally {
+        client.close();
+        chrome.kill();
+      }
+
+      done();
     });
   });
 });
