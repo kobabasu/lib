77d7ec87c61bb318de85da6070078ea3e48bb8cd
kobabasu <keiji.kobayashi.web@gmail.com>
2017-10-13 14:52:12 +0900

    coverage用に変更

--- test/UpdateCopyright.test.js
+++ test/UpdateCopyright.test.js
@@ -1,9 +1,11 @@
 import fs from 'fs'
+import { JSDOM } from 'jsdom'
 import { assert } from 'chai'
+import sinon from 'sinon'
 import { launch } from 'chrome-launcher'
 import CDP from 'chrome-remote-interface'
 import { dir } from '../gulp/dir.es6'
-import UpdateCopyright from '../modules/UpdateCopyright.js'
+import { UpdateCopyright } from '../modules/UpdateCopyright.js'
 
 const URL = 'about:blank';
 const HTML = './test/UpdateCopyright.test.html';
@@ -27,62 +29,42 @@ const launchChrome = async () => {
 
 describe('UpdateCopyright', () => {
 
-  it('クラスがなくともエラーとならないか', (done) => {
-
-    launchChrome().then(async (chrome) => {
-      const client = await CDP({ port: chrome.port });
-      const { Page, Runtime, DOM, Console } = client;
-      await Promise.all([
-        Page.enable(),
-        Runtime.enable(),
-        DOM.enable(),
-        Console.enable()
-      ]);
-
-      Console.messageAdded((msg) => console.log(msg));
-
-      await Page.addScriptToEvaluateOnLoad({
-        scriptSource: fetch(JS)
-      });
-
-      const frame = await Page.navigate({ url: URL });
-      Page.loadEventFired();
-
-      await Page.setDocumentContent({
-        frameId: frame.frameId,
-        html: fetch(HTML)
-      });
-
-      const body = await DOM.getDocument();
-      const el = await DOM.querySelector({
-        nodeId: body.root.nodeId,
-        selector: '.copyright'
-      });
-      await DOM.removeNode({ nodeId: el.nodeId });
-
-      const exp = `(() => {
-        const module = new UpdateCopyright();
-        return module.init();
-      })()`;
-      const res = await Runtime.evaluate({ expression: exp });
-      // console.log(res);
-
-      const thisyear = new Date().getFullYear();
-      try {
-        assert.notEqual(res.result.subtype, 'error');
-      } catch(error) {
-        return done(error);
-      } finally {
-        client.close();
-        chrome.kill();
-      }
-
-      done();
-    });
+  it('クラスが存在しない場合undefinedを返すか', () => {
+    const module = new UpdateCopyright();
+    assert.isUndefined(module.init());
   });
 
 
-  it('結果が今年となるか', (done) => {
+  it('クラスが存在する場合今年に変更されるか', () => {
+    const dom = new JSDOM(`
+      <span class="copyright">2000</span>
+    `);
+
+    global.document = dom.window.document;
+    const module = new UpdateCopyright();
+    module.init();
+
+    const el = dom.window.document
+      .querySelector('.copyright');
+    const res = el.innerHTML;
+    const thisyear = new Date().getFullYear();
+
+    assert.equal(res, thisyear);
+  });
+
+
+  it('debugがtrueの際エラーが表示されるか', () => {
+    const module = new UpdateCopyright();
+    const spy = sinon.spy(console, 'log');
+    const msg = 'UpdateCopyright: ';
+    module._debug = true;
+    module.log('test');
+
+    assert.isTrue(spy.calledWith(msg + 'test'));
+  });
+
+
+  it('E2E: 結果が今年となるか', (done) => {
 
     launchChrome().then(async (chrome) => {
       const client = await CDP({ port: chrome.port });
